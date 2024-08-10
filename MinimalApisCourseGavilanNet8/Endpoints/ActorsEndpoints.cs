f74bc75da18e2359b824e9d975a3fa01c9ef53b6
luisangeluis <luisangeluis@gmail.com>
2024-08-10 12:02:49 -0600

    Give a virtual credit card to see the video about azure

--- MinimalApisCourseGavilanNet8/Endpoints/ActorsEndpoints.cs
+++ MinimalApisCourseGavilanNet8/Endpoints/ActorsEndpoints.cs
@@ -5,11 +5,13 @@ using Microsoft.AspNetCore.OutputCaching;
 using MinimalApisCourseGavilanNet8.DTOs;
 using MinimalApisCourseGavilanNet8.Entities;
 using MinimalApisCourseGavilanNet8.Repositories;
+using MinimalApisCourseGavilanNet8.Services;
 
 namespace MinimalApisCourseGavilanNet8.Endpoints
 {
     public static class ActorsEndpoints
     {
+        private readonly static string container = "actors";
         public static RouteGroupBuilder MapActors(this RouteGroupBuilder group)
         {
             group.MapPost("/",Create).DisableAntiforgery();
@@ -17,9 +19,16 @@ namespace MinimalApisCourseGavilanNet8.Endpoints
         }
 
        static async Task<Created<ActorDTO>> Create([FromForm] CreateActorDTO createActorDTO,
-           IActorsRepository repository, IOutputCacheStore outputCacheStore, IMapper mapper)
+           IActorsRepository repository, IOutputCacheStore outputCacheStore, IMapper mapper,
+           IFileStorage fileStorage)
         {
             var actor = mapper.Map<Actor>(createActorDTO);
+
+            if (createActorDTO.Picture is not null) {
+                var url = await fileStorage.Store(container, createActorDTO.Picture);
+                actor.Picture = url;
+            }
+
             var id = await repository.Create(actor);
 
             await outputCacheStore.EvictByTagAsync("actors-get", default);
---
+++ MinimalApisCourseGavilanNet8/Services/LocalFileStorage.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MinimalApisCourseGavilanNet8.Services
+{
+    public class LocalFileStorage(IWebHostEnvironment env,
+        IHttpContextAccessor httpContextAccessor) : IFileStorage
+    {
+        public Task Delete(string? route, string container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Store(string container, IFormFile file)
+        {
+            var extension = Path.GetExtension(file.FileName);
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            string folder = Path.Combine(env.WebRootPath,container);
+        }
+    }
+}
